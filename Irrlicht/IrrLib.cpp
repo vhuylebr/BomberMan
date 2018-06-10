2c848db4472c3c18e76659d664e051aa9e844451
Sully <sully>
2018-06-10 22:42:06 +0200

    new char and maps

--- Irrlicht/IrrLib.cpp
+++ Irrlicht/IrrLib.cpp
@@ -550,13 +550,13 @@ void IrrLib::updatePlayer(std::unique_ptr<IEntity> &entity)
 
 void IrrLib::addPlayer(std::unique_ptr<IEntity> &entity)
 {
-	irr::scene::IAnimatedMesh* mesh = _smgr->getMesh("./media/Shadow.md2");
+	irr::scene::IAnimatedMesh* mesh = _smgr->getMesh("./media/EGGM.md2");
 	irr::scene::IAnimatedMeshSceneNode* node = _smgr->addAnimatedMeshSceneNode(mesh);
 	if (node) {
 		node->setMaterialFlag(irr::video::EMF_LIGHTING, false);
 		node->setMD2Animation(irr::scene::EMAT_STAND);
-		node->setMaterialTexture( 0, _driver->getTexture("./media/SHADOW.png") );
-		node->setScale(irr::core::vector3df(0.02f, 0.02f, 0.02f));
+		node->setMaterialTexture( 0, _driver->getTexture("./media/EGGM.PNG"));
+		node->setScale(irr::core::vector3df(0.01f, 0.01f, 0.01f));
 		node->setPosition(irr::core::vector3df(entity->getPos().first, 0.5, entity->getPos().second));
 		node->setID(static_cast<Player*>(entity.get())->getId());
 		_players.push_back(node);
